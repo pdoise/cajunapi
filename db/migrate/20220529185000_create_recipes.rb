31dc0d64c4998e470b8b16d3cfd08c970d66ff72
Phillip Doise <p.doise@gmail.com>
2022-05-29 14:44:15 -0600

    wrong key

--- db/migrate/20220529185000_create_recipes.rb
+++ db/migrate/20220529185000_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :username
+      t.string :name
       t.string :description
       t.string :ingredients
       t.string :imgsrc
