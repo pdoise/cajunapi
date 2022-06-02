820814104dccf09e87e688c64eaa40c2d5b77f7a
Phillip Doise <p.doise@gmail.com>
2022-06-01 18:55:30 -0600

    save

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,41 +7,28 @@ class RecipesController < ApplicationController
   
   def show
     @recipe = Recipe.find(params[:id])
-    render json: @recipe.as_json(include: :user)
+    render json: @recipe.as_json(include: :image, include: :user)
   end
 
   def create
     user = User.find_by_email('p.doise@gmail.com')
-    @recipe = Recipe.create(
-      user_id:     user.id             , 
-      name:        params[:name]       , 
-      description: params[:description], 
-      ingredients: params[:ingredients], 
-      directions:  params[:directions] ,
-      imgsrc:      params[:imgsrc]     ,
-      rating:      params[:rating]     ,
-      cooktime:    params[:cooktime]
-    )
+    @recipe = Recipe.create(recipe_params)
     render json: @recipe
   end
 
   def update
     @recipe = Recipe.find(params[:id])
-    @recipe.update(
-      name:        params[:name]       , 
-      description: params[:description], 
-      ingredients: params[:ingredients], 
-      directions:  params[:directions] ,
-      imgsrc:      params[:imgsrc]     ,
-      rating:      params[:rating]     ,
-      cooktime:    params[:cooktime]
-    )
+    @recipe.update(recipe_params)
     render json: @recipe
   end
 
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
+  end
+
+  def recipe_params
+    params.permit(:user_id, :name, :description, :ingredients, :directions, :imgsrc, :rating, :cooktime, :image)
   end
 
 end 
