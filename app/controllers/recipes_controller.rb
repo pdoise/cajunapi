94e44d29337f7223aca8e31fcbb1f6b2fb9ce7b3
Phillip Doise <p.doise@gmail.com>
2023-05-11 18:02:40 -0600

    updates

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,26 +2,28 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.includes(:user).with_attached_image.all
-    recipes_with_image_data = @recipes.map do |recipe|
-      if recipe.image.attached? && recipe.image.download
-        image_data = Base64.strict_encode64(recipe.image.download)
-        recipe.as_json(include: { user: { only: :username } }).merge(image: { content_type: recipe.image.content_type, data: image_data })
+    recipes_data = @recipes.map do |recipe|
+      recipe_hash = recipe.as_json(include: { user: { only: :username } })
+      if recipe.image.attached?
+        recipe_hash.merge(image: { 
+          content_type: recipe.image.content_type, 
+          data: Base64.strict_encode64(recipe.image.download) 
+        })
       else
-        recipe.as_json(include: { user: { only: :username } })
+        recipe_hash
       end
     end
-    render json: recipes_with_image_data
+    render json: recipes_data
   end
   
   def show
     @recipe = Recipe.includes(:user).with_attached_image.find(params[:id])
+    recipe_data = @recipe.as_json(include: :user)
     if @recipe.image.attached? && @recipe.image.download
-      data = @recipe.image.download
-      image_data = Base64.strict_encode64(data)
-      render json: @recipe.as_json(include: :user).merge(image: { content_type: @recipe.image.content_type, data: image_data })
-    else
-      render json: @recipe.as_json(include: :user)
+      image_data = Base64.strict_encode64(@recipe.image.download)
+      recipe_data.merge!(image: { content_type: @recipe.image.content_type, data: image_data })
     end
+    render json: recipe_data
   end
 
 
