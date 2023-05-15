47666fb6bf4c243373b38cb78b4df4d5ef0b046f
Phillip Doise <p.doise@gmail.com>
2023-05-14 18:38:58 -0600

    foo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::API
     if @headers['Authorization'].present?
       token = @headers['Authorization'].split(' ').last
       decoded_token = decode(token)
-      puts "*"*1000
-      puts Rails.application.secrets.secret_key_base
       user = User.find_by(id: decoded_token[:user_id])
       render json:{ error: 'Not Authorized' }, status: 401 unless user
     else
@@ -23,7 +21,14 @@ class ApplicationController < ActionController::API
   end
 
   def decode(token)
-    body = JWT.decode(token,Rails.application.secrets.secret_key_base)[0]
+    body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    puts "*"*1000
+    puts 'secret key'
+    puts Rails.application.secrets.secret_key_base
+    puts 'body'
+    puts body
+    puts 'hash' 
+    puts HashWithIndifferentAccess.new body
     HashWithIndifferentAccess.new body
   rescue
     nil
