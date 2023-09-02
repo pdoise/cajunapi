5b61a6c63b4e846596601a2a2f67275df511f373
Phillip Doise <p.doise@gmail.com>
2023-09-02 14:44:58 -0500

    save

---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,12 @@
+#class RecipeSerializer < ActiveModel::Serializer
+#  attributes :id
+#  attributes :user_id
+#  attributes :name
+#  attributes :description
+#  attributes :ingredients
+#  attributes :directions
+#  attributes :rating
+#  attributes :cooktime
+#  attributes :image
+#  include Rails.application.routes.url_helpers
+#end
