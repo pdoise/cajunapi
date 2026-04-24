550fb9948ebd317842effdab574095b7454df3dd
Phillip Doise <p.doise@gmail.com>
2026-04-24 17:08:47 -0500

    save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def landing
-    @featured_recipes = Recipe.with_attached_image.includes(:user).order(created_at: :desc).limit(6)
+    @featured_recipes = Recipe.joins(:image_attachment).with_attached_image.includes(:user).order(created_at: :desc).limit(6)
   end
 
   def contact
